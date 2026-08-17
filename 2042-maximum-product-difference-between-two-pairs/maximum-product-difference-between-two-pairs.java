5d9655bd4252f3303f3e817123798c572724d865
Abhay3170 <abhay3170.beai25@chitkara.edu.in>
2026-08-17 18:34:40 +0530

    Time: 3 ms (75.52%) | Memory: 46.7 MB (91.62%) - LeetSync

---
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProductDifference(int[] nums) {
+        int s=Integer.MAX_VALUE;
+        int ss=Integer.MAX_VALUE;
+        int l=Integer.MIN_VALUE;
+        int sl=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>l){
+                sl=l;
+                l=nums[i];
+            }
+            else if(nums[i]>sl){
+                sl=nums[i];
+            }
+            if(nums[i]<s){
+                ss=s;
+                s=nums[i];
+            }
+            else if(nums[i]<ss){
+                ss=nums[i];
+            }
+        }
+        return l*sl-s*ss;
+    }
+}
