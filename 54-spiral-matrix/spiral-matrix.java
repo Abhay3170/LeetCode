c3339e36a465edd956e8431dff3a5bc9b9bc526b
Abhay3170 <abhay3170.beai25@chitkara.edu.in>
2026-08-19 09:31:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (25.71%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int rs=0,re=matrix.length-1,cs=0,ce=matrix[0].length-1;
+        List<Integer> res=new ArrayList<>();
+        while(rs<=re && cs<=ce){
+            for(int i=cs;i<=ce;i++){
+                res.add(matrix[rs][i]);
+            }
+                rs++;
+            for(int i=rs;i<=re;i++){
+                res.add(matrix[i][ce]);
+            }
+                ce--;
+            if(rs<=re){
+                for(int i=ce;i>=cs;i--){
+                    res.add(matrix[re][i]);
+            }
+                re--;
+            }
+            if(cs<=ce){
+                for(int i=re;i>=rs;i--){
+                    res.add(matrix[i][cs]);
+            }
+                cs++;
+            }
+        }
+        return res;
+    }
+}
