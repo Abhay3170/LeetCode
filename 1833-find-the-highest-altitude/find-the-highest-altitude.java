b02b9f6f31b30e9a861af885fc7179bd14814485
Abhay3170 <abhay3170.beai25@chitkara.edu.in>
2026-08-20 09:28:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (95.09%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max=0;
+        int s=0;
+        for(int i=0;i<gain.length;i++){
+            s+=gain[i];
+            max=Math.max(max,s);
+            
+        }
+        
+        return max;
+    }
+}
