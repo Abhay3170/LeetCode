ebd0e376b619aea18121d7d4d3d287421a0512b8
Abhay3170 <abhay3170.beai25@chitkara.edu.in>
2026-08-27 09:41:27 +0530

    Time: 1 ms (98.91%) | Memory: 44.5 MB (88.15%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,32 +1,18 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        long l=0,sl=0,tl=0;
-        if(nums.length==1)return nums[0];
-        if(nums[0]>nums[1]){
-            l=nums[0];
-            sl=nums[1];
-        }
-        else if(nums[0]<nums[1]){
-            l=nums[1];
-            sl=nums[0];
-        }
-        else{
-            l=nums[0];
-            sl=Long.MIN_VALUE;
-        }
-        tl=Long.MIN_VALUE;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]>l){
+        long l=Long.MIN_VALUE,sl=Long.MIN_VALUE,tl=Long.MIN_VALUE;
+        for(int num:nums){
+            if(num>l){
                 tl=sl;
                 sl=l;
-                l=nums[i];
+                l=num;
             }
-            else if(nums[i]>sl && nums[i]!=l){
+            else if(num>sl && num!=l){
                 tl=sl;
-                sl=nums[i];
+                sl=num;
             }
-            else if(nums[i]>tl && nums[i]!=sl && nums[i]!=l){
-                tl=nums[i];
+            else if(num>tl && num!=sl && num!=l){
+                tl=num;
             }
         }
         if(tl==Long.MIN_VALUE)return (int)(l);
