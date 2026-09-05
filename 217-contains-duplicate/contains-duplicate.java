2cb53e75efa9ad3c8208c09bd152883310b712e0
Abhay3170 <abhay3170.beai25@chitkara.edu.in>
2026-09-05 22:29:16 +0530

    Time: 19 ms (48.67%) | Memory: 108.1 MB (47.04%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,8 +1,9 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==nums[i-1])return true;
+        HashSet<Integer> set=new HashSet<>();
+        for(int i:nums){
+            if(set.contains(i))return true;
+            set.add(i);
         }
         return false;
     }
