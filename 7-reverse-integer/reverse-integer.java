cd90da64fab28b1b9034c88ada92e44300cadd53
Abhay3170 <abhay3170.beai25@chitkara.edu.in>
2026-09-06 10:02:36 +0530

    Time: 1 ms (99.97%) | Memory: 42.8 MB (26.56%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverse(int x) {
+        long s=0;
+        while(x!=0){
+            s=s*10+(x%10);
+            x/=10;
+        }
+        if(s>Integer.MAX_VALUE || s<Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int) s;
+    }
+}
