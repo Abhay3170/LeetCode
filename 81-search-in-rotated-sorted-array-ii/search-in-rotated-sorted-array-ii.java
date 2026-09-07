c082dee8e63fd5c40cf930f1615313e09ec41c5f
Abhay3170 <abhay3170.beai25@chitkara.edu.in>
2026-09-07 12:06:11 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (81.91%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean search(int[] nums, int target) {
         int i=0,j=nums.length-1;
-        while(i<=j){
-            if(nums[i]==target || nums[j]==target)return true;
-            i++;j--;
-        }
+        while(i<=j)
+            if(nums[i++]==target || nums[j--]==target)return true;
         return false;
     }
 }
