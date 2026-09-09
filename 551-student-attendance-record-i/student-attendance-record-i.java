6b1db7bb1f00265553ec3b37f687aff4d84ee5ff
Abhay3170 <abhay3170.beai25@chitkara.edu.in>
2026-09-09 12:20:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (59.85%) - LeetSync

--- 551-student-attendance-record-i/student-attendance-record-i.java
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean checkRecord(String s) {
-        return !s.contains("LLL") && s.indexOf("A")==s.lastIndexOf("A");
+        int a=0,l=0;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='A'){
+                a++;
+                l=0;
+                if(a>1)return false;
+            }
+            else if(c=='L'){
+                l++;
+                if(l>2)return false;
+            }
+            else l=0;
+        }
+        return true;
     }
 }
