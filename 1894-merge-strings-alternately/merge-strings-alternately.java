d2374ee71a86d6f50aafe7c4bb2ae4415924c3de
Abhay3170 <abhay3170.beai25@chitkara.edu.in>
2026-09-13 13:30:17 +0530

    Time: 1 ms (95.84%) | Memory: 43.3 MB (34.53%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,12 +1,17 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder s=new StringBuilder();
-        int i=0,j=0;
-        while(i<word1.length() || j<word2.length()){
-            if(i<word1.length())s.append(word1.charAt(i++));
-            if(j<word2.length())s.append(word2.charAt(j++));
+        StringBuilder ans = new StringBuilder();
+        int length = Math.min(word1.length(), word2.length());
+        for(int i = 0; i<length; i++){
+            ans.append(word1.charAt(i));
+            ans.append(word2.charAt(i));
         }
-        return s.toString();
-
+        if(word1.length() > word2.length()){
+            ans.append(word1.substring(length , word1.length()));
+        }
+        if(word2.length() > word1.length()){
+            ans.append(word2.substring(length , word2.length()));
+        }
+        return ans.toString();
     }
 }
