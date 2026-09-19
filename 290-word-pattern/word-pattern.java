4a3fadbfac7b36a10b2f9a63b7d55dc340c7daf6
Abhay3170 <abhay3170.beai25@chitkara.edu.in>
2026-09-19 11:51:13 +0530

    Time: 1 ms (83.81%) | Memory: 42.5 MB (87.07%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,17 +1,38 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        Map<Character,String> map = new HashMap<>();
-        String arr[]=s.split("\\s+");
-        if(pattern.length()!=arr.length)return false;
-        for(int i=0;i<pattern.length();i++){
-            if(map.containsKey(pattern.charAt(i))){
-                if(!map.get(pattern.charAt(i)).equals(arr[i]))return false;
+        String[] words = s.split(" ");
+
+        // Number of characters in pattern must equal number of words in s
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            // Check mapping from char to word
+            if (charToWord.containsKey(c)) {
+                if (!charToWord.get(c).equals(word)) {
+                    return false;
+                }
+            } else {
+                charToWord.put(c, word);
             }
-            else{
-                if(map.containsValue(arr[i]))return false;
-                map.put(pattern.charAt(i),arr[i]);
+
+            // Check mapping from word to char
+            if (wordToChar.containsKey(word)) {
+                if (wordToChar.get(word) != c) {
+                    return false;
+                }
+            } else {
+                wordToChar.put(word, c);
             }
         }
+
         return true;
     }
 }
