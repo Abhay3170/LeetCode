0dca4c11c4d98514bed8f734d43908a2a4cf4831
Abhay3170 <abhay3170.beai25@chitkara.edu.in>
2026-09-20 12:51:39 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (67.44%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum=0;
+        for(int i=1;i<=s.length();i++){
+            char c=s.charAt(i-1);
+            int x=26-(s.charAt(i-1)-'a');
+            sum+=x*i;
+        }
+        return sum;
+    }
+}
