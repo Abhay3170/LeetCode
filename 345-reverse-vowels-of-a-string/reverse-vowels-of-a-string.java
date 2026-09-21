2e3eb15568ba71097a5122c134edfa10cf36f981
Abhay3170 <abhay3170.beai25@chitkara.edu.in>
2026-09-21 18:37:56 +0530

    Time: 3 ms (62.19%) | Memory: 46.5 MB (79.28%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,19 +1,25 @@
 class Solution {
     public String reverseVowels(String s) {
-        int i=0,j=s.length()-1;
-        char arr[]=s.toCharArray();
-        while(i<j){
-            if(!isVowel(arr[i]))i++;
-            if(!isVowel(arr[j]))j--;
-            if(isVowel(arr[i]) && isVowel(arr[j])){
-                char temp=arr[i];
-                arr[i++]=arr[j];
-                arr[j--]=temp;
+        int left=0;
+        int right=s.length()-1;
+        char[] arr=s.toCharArray();
+        while(left<right){
+            while(left<right&&!isvowel(s.charAt(left))){
+                left++;
             }
+            while(left<right&&!isvowel(s.charAt(right))){
+                right--;
+            }
+            char temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
         }
-        return new String(arr);
+        String result=new String(arr);
+        return result;
     }
-    public static boolean isVowel(char c){
+    public static boolean isvowel(char c){
         c=Character.toLowerCase(c);
         if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')return true;
         return false;
