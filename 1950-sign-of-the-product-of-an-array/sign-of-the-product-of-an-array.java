e0fd51964ce65aa7d604834e5abe958d43d98fe6
Abhay3170 <abhay3170.beai25@chitkara.edu.in>
2026-09-21 09:46:14 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (6.20%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -1,13 +1,9 @@
 class Solution {
     public int arraySign(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0)return 0;
-            else if(nums[i]>0)nums[i]=1;
-            else nums[i]=-1;
-        }
         int p=1;
-        for(int i=0;i<nums.length;i++){
-            p*=nums[i];
+        for(int x:nums){
+            if(x==0)return 0;
+            else if(x<0)p=-p;
         }
         return p;
     }
