41a576603106b8e482aad98cfdf27c17594bbdd5
Abhay3170 <abhay3170.beai25@chitkara.edu.in>
2026-09-23 18:34:56 +0530

    Time: 8 ms (48.57%) | Memory: 44.2 MB (48.47%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length())return false;
+        int a[]=new int [26];
+        int b[]=new int [26];
+        for(int i=0;i<s1.length();i++){
+            a[s1.charAt(i)-'a']++;
+            b[s2.charAt(i)-'a']++;
+        }
+        for(int i=s1.length();i<s2.length();i++){
+            if(matches(a,b))return true;
+            b[s2.charAt(i-s1.length())-'a']--;
+            b[s2.charAt(i)-'a']++;
+        }
+        return matches(a,b);
+    }
+    public boolean matches(int a[],int b[]){
+        for(int i=0;i<26;i++){
+            if(a[i]!=b[i])return false;
+        }
+        return true;
+    }
+}
